11a7b1dda5a0f7c02f17b04a798b87e748768bc0
emrbalc1 <emrbalc160@gmail.com>
2023-07-09 16:35:07 +0300

    Memo örneği yapıldı

--- src/App.js
+++ src/App.js
@@ -1,7 +1,29 @@
-const App = ()=> {
+import React from "react";
+import Todos from "./Todos";
+
+function App() {
+  
+  const [count, setCount] = React.useState(0);
+  const [todos, setTodos] = React.useState(["todo 1", "todo 2"]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+    // setTodos([...todos, "Yapılacak işler 3"])
+  }
+
+  return (
     <>
-        Merhaba
+      <Todos todos={todos} />
+
+      <hr />
+
+      <div>
+        Sayaç: {count}
+        <button onClick={increment}>+</button>
+      </div>
     </>
+  )
+
 }
 
-export default App
+export default App;
