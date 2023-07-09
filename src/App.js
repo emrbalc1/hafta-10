ca17df9da4fd599f901147cb8c67e56d600592a2
emrbalc1 <emrbalc160@gmail.com>
2023-07-09 16:57:14 +0300

    usecallback örneği yapıldı

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-const App = ()=> {
-    return (
-        
-    )
-}
+import { useState, useCallback } from "react";
+import Todos from "./Todos";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const increment = () => {
+    setCount((c) => c + 1);
+  };
+  const addTodo = useCallback( () => {
+    setTodos((t) => [...t, "New Todo"]);
+  }, [todos])
+
+  return (
+    <>
+      <Todos todos={todos} addTodo={addTodo} />
+      <hr />
+      <div>
+        Count: {count}
+        <button onClick={increment}>+</button>
+      </div>
+    </>
+  );
+};
 
 export default App
